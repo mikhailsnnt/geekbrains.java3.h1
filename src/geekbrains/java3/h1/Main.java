939b153709e67d87ff7e35bf24a387008f2dfb14
Mihail Nikitin <nitinin.miha@icloud.com>
2021-11-11 10:16:32 +0300

    Initial commit

---
+++ src/geekbrains/java3/h1/Main.java
@@ -0,0 +1,16 @@
+package geekbrains.java3.h1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+
+    }
+
+
+}
