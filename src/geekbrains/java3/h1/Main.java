ce8592ddadb5afe7358ba70bbf9373cf785963be
Mihail Nikitin <nitinin.miha@icloud.com>
2021-11-11 10:20:08 +0300

    First 2 tasks.

--- src/geekbrains/java3/h1/Main.java
+++ src/geekbrains/java3/h1/Main.java
@@ -11,6 +11,17 @@ public class Main {
 
 
     }
-
+    private static <T> void  swapElements(T[] array, int ind, int ind2)
+    {
+        if(Math.min(ind,ind2) < 0 || Math.max(ind,ind2) >= array.length)
+            throw new IndexOutOfBoundsException();
+        T el1 = array[ind];
+        array[ind] = array[ind2];
+        array[ind2] = el1;
+    }
+    private static <T> ArrayList<T> toArrayList(T[]array)
+    {
+        return new ArrayList<>(Arrays.asList(array));
+    }
 
 }
